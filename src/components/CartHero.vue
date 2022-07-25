952592f2a6913993fc44514a5cfe209a65a3f228
Emilsone <lawsonezekiel68@gmail.com>
2022-07-25 16:50:34 +0100

    create cart page

---
+++ src/components/CartHero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container py-24 px-5 mx-auto my-12">
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1
+        class="sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900"
+      >
+        Your cart is currently empty.
+      </h1>
+      <p class="lg:w-1/2 mx-auto leading-relaxed text-base text-gray-900">
+      Before you proceed to checkout you must add some products to your shopping cart.You will find a lot of interesting products on our Shop page.
+      </p>
+
+       <div class=" pt-12 w-full">
+      <i class="fa fa-shopping-cart text-black text-9xl opacity-20" aria-hidden="true"></i>
+
+      </div>
+    </div>
+  </div>
+</template>
