67a42331ffe30d116fae2790c268abc9a111da3b
Emilsone <lawsonezekiel68@gmail.com>
2022-07-25 16:40:46 +0100

    add cart icon, and wish page icon

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,16 @@
 <template>
   <nav :class="headerClassList" class="fixed w-full z-30 top-0 header___bar">
     <div
-      class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2"
+      class="
+        w-full
+        container
+        mx-auto
+        flex flex-wrap
+        items-center
+        justify-between
+        mt-0
+        py-2
+      "
     >
       <div class="pl-4 flex items-center">
         <logo :isStickable="true" :isSticky="isSticky" />
@@ -31,80 +40,167 @@
 
       <div
         :class="navContentClassList"
-        class="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+        class="
+          w-full
+          flex-grow
+          lg:flex lg:items-center lg:w-auto lg:block
+          mt-2
+          lg:mt-0
+          bg-white
+          lg:bg-transparent
+          text-black
+          p-4
+          lg:p-0
+          z-20
+        "
       >
         <ul class="list-reset lg:flex justify-center flex-1 items-center">
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
               ><router-link to="/">ABOUT US </router-link></a
             >
           </li>
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
               ><router-link to="/">FARM PRODUCTS </router-link></a
             >
           </li>
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
-              ><router-link to="/farmers"
-                >OUR FARMERS</router-link
-            ></a>
+              ><router-link to="/farmers">OUR FARMERS</router-link></a
+            >
           </li>
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
               ><router-link to="/faq">FAQ <i class="icon-map"></i></router-link
             ></a>
           </li>
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
               ><router-link to="/blog">BLOG</router-link></a
             >
           </li>
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
               ><router-link to="/contact">CONTACT US</router-link></a
             >
           </li>
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
               ><router-link to="/wishlistpage">
-                <i
-                  class="fa fa-heart-o text-lg"
-                  aria-hidden="true"
-                ></i></router-link
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  /></svg></router-link
             ></a>
           </li>
         </ul>
         <ul class="list-reset lg:flex items-center">
           <li class="mr-3">
             <a
-              class="inline-block no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              class="
+                inline-block
+                no-underline
+                hover:text-gray-800 hover:text-underline
+                py-2
+                px-4
+              "
               href="#"
               ><router-link to="/cartpage">
-                <i
-                  class="fa fa-shopping-bag text-2xl"
-                  aria-hidden="true"
-                ></i></router-link
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  /></svg></router-link
             ></a>
           </li>
 
           <li class="">
             <a
-              class="inline-block text-white shadow-2xl btn-sign px-10 no-underline hover:text-underline py-3 md:mx-6"
+              class="
+                inline-block
+                text-white
+                shadow-2xl
+                btn-sign
+                px-10
+                no-underline
+                hover:text-underline
+                py-3
+                md:mx-6
+              "
               href="#"
               ><router-link to="/signin"> Sign In </router-link></a
             >
