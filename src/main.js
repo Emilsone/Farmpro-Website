28e9e711b3133a3838d68a0713db85f97ad948fc
Your Name Here <lawsonezekiel68@gmail.com>
2024-03-13 16:31:49 +0100

    add sentry file

--- src/main.js
+++ src/main.js
@@ -4,10 +4,29 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/styles/index.css'
-
+import * as Sentry from "@sentry/vue";
 
 
 Vue.config.productionTip = false
+
+Sentry.init({
+  Vue,
+  dsn: "https://51b32b3d40ea5d78f2402724f527a5a9@o4506904220598272.ingest.us.sentry.io/4506904288821248",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 new Vue({
   router,
