9b8b5210ef184983f638138084787d20fe745184
Your Name Here <lawsonezekiel68@gmail.com>
2024-03-14 10:20:18 +0100

    refix header nav

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,6 @@
 <template>
-  <nav :class="headerClassList" class="fixed w-full z-30 top-0 header___bar">
-    <div
-      class="
+  <nav :class="headerClassList" class="fixed w-full z-30 top-0 header___bar bg-white">
+    <div class="
         w-full
         container
         mx-auto
@@ -10,187 +9,119 @@
         justify-between
         mt-0
         py-2
-      "
-    >
+      ">
       <div class="pl-4 flex items-center">
         <logo :isStickable="true" :isSticky="isSticky" />
       </div>
       <div class="block lg:hidden pr-4">
-        <span
-          class="flex items-center p-1 text-black hover:text-gray-900"
-          @click.prevent.stop="onToggleClick"
-        >
-          <svg
-            class="h-8 w-8"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4 6H20M4 12H20M4 18H11"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
+        <span class="flex items-center p-1 text-black hover:text-gray-900" @click.prevent.stop="onToggleClick">
+          <svg class="h-8 w-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+              stroke-linejoin="round" />
             <title>Menu</title>
           </svg>
         </span>
       </div>
 
-      <div
-        :class="navContentClassList"
-        class="
+      <div :class="navContentClassList" class="
           w-full
           flex-grow
           lg:flex lg:items-center lg:w-auto lg:block
           mt-2
           lg:mt-0
-          bg-white
-          lg:bg-transparent
           text-black
           p-4
           lg:p-0
           z-20
-        "
-      >
+        ">
         <ul class="list-reset lg:flex justify-center flex-1 items-center">
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/about">ABOUT US </router-link></a
-            >
+              " href="#"><router-link to="/about">ABOUT US </router-link></a>
           </li>
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/farmproducts">FARM PRODUCTS </router-link></a
-            >
+              " href="#"><router-link to="/farmproducts">FARM PRODUCTS </router-link></a>
           </li>
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/farmers">OUR FARMERS</router-link></a
-            >
+              " href="#"><router-link to="/farmers">OUR FARMERS</router-link></a>
           </li>
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/faq">FAQ <i class="icon-map"></i></router-link
-            ></a>
+              " href="#"><router-link to="/faq">FAQ <i class="icon-map"></i></router-link></a>
           </li>
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/blog">BLOG</router-link></a
-            >
+              " href="#"><router-link to="/blog">BLOG</router-link></a>
           </li>
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/contact">CONTACT US</router-link></a
-            >
+              " href="#"><router-link to="/contact">CONTACT US</router-link></a>
           </li>
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/wishlist">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  /></svg></router-link
-            ></a>
+              " href="#"><router-link to="/wishlist">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                  stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                </svg></router-link></a>
           </li>
         </ul>
         <ul class="list-reset lg:flex items-center">
           <li class="mr-3">
-            <a
-              class="
+            <a class="
                 inline-block
                 no-underline
                  hover:text-underline
                 py-2
                 px-4
-              "
-              href="#"
-              ><router-link to="/cart">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  /></svg></router-link
-            ></a>
+              " href="#"><router-link to="/cart">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                  stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg></router-link></a>
           </li>
 
           <li class="">
-            <a
-              class="
+            <a class="
                 inline-block
                 text-white
                 shadow-2xl
@@ -200,10 +131,7 @@
                 hover:text-underline
                 py-3
                 md:mx-6
-              "
-              href="#"
-              ><router-link to="/signin"> Sign In </router-link></a
-            >
+              " href="#"><router-link to="/signin"> Sign In </router-link></a>
           </li>
         </ul>
       </div>
@@ -214,8 +142,6 @@
 
 <script>
 import HeaderLogo from "@/components/HeaderLogo";
-// import database from "../services/database";
-
 export default {
   name: "TheHeader",
   components: {
@@ -228,20 +154,14 @@ export default {
     };
   },
   computed: {
-    // currentUser() {
-    //   return this.$store.state.currentUser;
-    // },
     isSticky() {
       return this.scrollY > 10;
     },
     headerClassList() {
       return this.isSticky ? "bg-white shadow" : "";
     },
-    navActionClassList() {
-      return this.isSticky ? "gradient text-white" : "bg-white text-white";
-    },
     navContentClassList() {
-      let classList = this.isSticky ? "bg-white" : "bg-gray-100";
+      let classList = this.isSticky ? "bg-white" : "";
       if (!this.isOpen) {
         classList += ` hidden`;
       }
