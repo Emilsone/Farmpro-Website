c2c0e99371c52fecc7255494e29317f2cd5b498c
Your Name Here <lawsonezekiel68@gmail.com>
2024-04-30 19:35:05 +0100

    Deleted the padding styles

--- src/components/DeliveryProcess.vue
+++ src/components/DeliveryProcess.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="text-gray-600 body-font bg-section mt-24">
-    <div class="container px-5 py-24 mx-auto">
+    <div class="container py-24 mx-auto">
       <div class="text-center mb-20">
         <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text font-bold">
           ~~ Delivery Process ~~
